3150d6a9d9c0ce1fca282e34413d42639c5dbeb7
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-04-17 10:37:05 +0530

    update

---
+++ Day-1/script.js
@@ -0,0 +1,65 @@
+// 1. Data 
+
+// data is information of anything - furkan , awdiz, 
+
+
+// 2. Datatypes 
+
+// name - ali - string
+// age  - 20 -  number 
+// yes/no     - boolean 
+// confused  - undefined
+
+
+// furkan
+// 8765
+// hii 
+// welcome 
+// log out
+
+// 1. String  
+
+// syntax - rules 
+// "", '' , ``
+// kuchbhi  
+// kuchbhi 
+// "kuchbhi igvfybnhawd " -> string 
+// 'welcome' -> string 
+// `logout`  -> string 
+
+
+// 2. number 
+
+// syntax 
+// 12 
+// 123
+// 3423
+// 121
+// 4232
+
+// 3. Boolean  
+
+// syntax  
+
+// true false 
+
+// 4. undefined 
+
+// syntax 
+
+
+// Varibles 
+
+// storage 
+
+// var let const 
+
+// cupboard clothing = shirt
+
+var myName = "furkan" ;
+var myAge = 20;
+var myStudentCompletedAssignment = true;
+
+console.log(myName)
+console.log(myAge)
+console.log(myStudentCompletedAssignment)
