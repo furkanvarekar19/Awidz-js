e48bca5b50c4a3dcb3fb0ffc112e17a888c28dab
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-04-17 22:37:37 +0530

    updated day-2

---
+++ Day-2/index.js
@@ -0,0 +1,33 @@
+// Using var 
+var favNumber = 18;  // declare
+// favNumber = 45;  //re assigned
+// var favNumber = 55; // re-declare
+
+console.log(favNumber);
+
+// Using let 
+let favNumber1 = 38;  // declare
+// favNumber1 = 45;  //re assigned
+// let favNumber1 = 55; // re-declare
+
+console.log(favNumber1);
+
+
+// Using const 
+const favNumber2 = 58;  // declare
+// favNumber2 = 45;  //re assigned
+// const favNumber2 = 55; // re-declare
+
+console.log(favNumber2);
+
+let num = 55;
+let num1= 35;
+
+console.log(num + num1);
+console.log(num - num1);
+console.log(num % num1);
+console.log(num * num1);
+console.log(num / num1);
+
+
+
