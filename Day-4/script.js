8c1c07ada740dbe9b628916cbce9e26dbd49889c
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-04-19 23:33:50 +0530

    update

--- Day-4/script.js
+++ Day-4/script.js
@@ -1,10 +1,12 @@
+// Q. Given an number find out the number is even or odd. 
+// if number is even then print "Even" and if number is odd then print "Odd"
+// var number = 345; Use if else conditional statement .
 
-// var  number = 4;
-// if(number % 2 == 0) {
-//     console.log("The number is even.");
-// }
+var  number = 4;
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+else {
+    console.log("The number is odd.");
+}
 
-// // if the number is odd
-// else {
-//     console.log("The number is odd.");
-// }
