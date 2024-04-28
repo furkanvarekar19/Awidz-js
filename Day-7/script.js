0460fc87635ccceae43fec5d72997b4558878264
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-04-28 13:02:26 +0530

    updated

---
+++ Day-7/script.js
@@ -0,0 +1,33 @@
+// function CheckNumberIsEvenOrOdd(number) {
+//     if (number % 2 === 0) {
+//       console.log("Even");
+//     } else {
+//       console.log("Odd");
+//     }
+//   }
+//   CheckNumberIsEvenOrOdd(20);
+
+
+  // Array
+
+//   var students = ["Virat", "Rohit", "Rahul"];
+// console.log(students, "students");
+// console.log(students.length); 
+// console.log(students[0]);
+// console.log(students[1]);
+// console.log(students[students.length - 1]);
+
+
+// Loops :
+// for
+
+// for( var i = 1; i<=10; i++){
+//     console.log(i);
+// }
+
+// Q . Print number from 33 to 76 with gap of 3 but number also must be even
+for (var i = 33; i <= 76; i += 3) {
+    if (i % 2 == 0) {
+      console.log(i);
+    }
+  }
