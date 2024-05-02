e8ad985def7095476ac9ea4ecd10bf1e14952ae6
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-05-02 14:38:09 +0530

    update

--- Day-9/script.js
+++ Day-9/script.js
@@ -43,14 +43,22 @@
 // }
 
 // Q Given an array, find two numbers whose addition is target.
-
-var array = [1, 2, 3, 4, 5, 6 ];
-var target = 5;
-console.log(array);
 var count = 0;
-for (var i = 0; i < array.length; i++) {
-  for (var j = i + 1; j < array.length; j++) { 
-    console.log(array[i], "+", array[j], "=", array[i] + array[j]);
+var array = [1, 2, 3, 4, 5, 6];
+var target = 5;
+console.log(array)
 
-  }
+for(var i=0; i<array.length; i++)
+{
+   for(var j=i+1; j<array.length; j++){
+   //  console.log(array[i],"+", array[j], "=", array[i] + array[j]);
+
+    if(array[i]+array[j]===target){
+      console.log(array[i],array[j]);
+
+    }
+   }
 }
+
+
+// console.log(count)
