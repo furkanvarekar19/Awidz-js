5fd4d40916f958d1904e3d8e698e440901176798
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-05-04 18:53:24 +0530

    update

---
+++ Day-10/script.js
@@ -0,0 +1,36 @@
+
+// const myfunction = (message) =>{
+//     console.log("functions arrow");
+//     }
+//     myfunction()
+
+// const month=["2","3000","55",8];
+// month.sort()
+// console.log(month);
+
+
+// const array2 = [1, 30, 4, 21, 100000];
+// array2.sort();
+// console.log(array2);
+
+// const array1 = [1, 30, 4, 21, 100000];
+// array1.sort((a, b) => a - b);
+// array1.sort((a, b) => b - a);
+// console.log(array1);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.splice(2, 0, "Lemon", "Kiwi");
+// console.log(fruits);
+
+
+var array = [1,2,3]
+array.push(4);
+console.log(array);
+
+const fruits1 = ["Banana", "Orange", "Apple", "Mango"];
+fruits1.push("Lemon");
+console.log(fruits1);
+
+const fruits2 = ["Banana", "Orange", "Apple", "Mango"];
+fruits2.pop();
+console.log(fruits2);
