4208efe8178cc016b3b56de96d055ada41028b79
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-05-10 15:05:02 +0530

    update

--- Day-12/script.js
+++ Day-12/script.js
@@ -1,7 +1,70 @@
-var array = [1];
-array [2] = 2;
-console.log(array);
+// var array = [1];
+// array [2] = 2;
+// console.log(array);
 
-var myObject = {name:"Awdiz"};
-myObject["age"] = 22;
-console.log(myObject);
+// var myObject = {name:"Awdiz"};
+// myObject["age"] = 22;
+// console.log(myObject);
+
+
+// Question 1: How do you create an empty array in JavaScript?
+
+// let array = []
+
+// console.log(array);
+
+// Question 2: How do you access the first and last elements of an array?
+// let arr = [1, 2, 3, 4];
+//  let first = arr[0];
+//  let second = arr[3]
+//  console.log(arr);
+//  console.log(first);
+//  console.log(second);
+
+// Question 3: How do you add an element to the end of an array?
+
+// let arr = [1, 2, 3, 4];
+// arr.push(5);
+// console.log(arr);
+
+// Question 4: How do you remove the last element from an array?
+
+// let arr = [1, 2, 3, 4];
+// arr.pop(3)
+// console.log(arr);
+
+// Question 5: How do you loop through an array in JavaScript?
+
+// let arr = [1, 2, 3, 4];
+// for(let i = 0; i<arr.length;i++){
+//     console.log(arr);
+
+// }
+
+// Question 6: How do you check if an element exists in an array?
+
+// let arr = [1, 2, 3, 4];
+// if (arr.indexOf(5) !== -1) {
+
+//     console.log('Element found');
+    
+//     } else {
+    
+//     console.log('Element not found');
+    
+//     }
+
+// Question 7: How do you remove an element from an array at a specific index?
+
+// let arr = [1, 2, 3];
+
+// arr.splice(2, 1);
+
+// console.log(arr); 
+
+// Array Reverse Using an Extra Array
+
+// let arr = [1, 2, 3,4,5];
+// console.log(arr);
+// let Reverse = arr.reverse();
+// console.log(arr);
